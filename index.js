4feaae455df1237d10161753f1eb90691dd8a324
Oscar Peulicke <oscarrpeulicke@gmail.com>
2019-07-27 22:01:47 +0200

    test

--- index.js
+++ index.js
@@ -8,7 +8,29 @@ app.use(express.static(__dirname + '/www'));
 var server = http.createServer(app);
 var wss = new WebSocket.Server({server});
 
+wss.getUniqueID = function () {
+    function s4() {
+        return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
+    }
+    return s4()+s4()+s4();
+};
+
+var clients = {};
+
+setInterval(function(){
+    var res = [];
+    wss.clients.forEach(function(client) {
+        res.push(client.data);
+    });
+    res = JSON.stringify(res);
+    wss.clients.forEach(function(client) {
+        client.send(res);
+    });
+}, 0);
+
 wss.on('connection', function(ws) {
+    ws.id = wss.getUniqueID();
+    clients[ws.id] = ws;
     ws.data = {};
     ws.on('message', function(msg) {
         var data = JSON.parse(msg);
@@ -16,16 +38,6 @@ wss.on('connection', function(ws) {
             ws.data[Object.keys(data)[i]] = data[Object.keys(data)[i]];
         }
     });
-    setInterval(function(){
-        var res = [];
-        wss.clients.forEach(function(client) {
-            res.push(client.data);
-        });
-        res = JSON.stringify(res);
-        wss.clients.forEach(function(client) {
-            client.send(res);
-        });
-    }, 100);
 });
 
 server.listen(process.env.PORT || 5000);
