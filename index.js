cb524652f95c3a332e3694bfa38efd41018760f4
Oscar Peulicke <oscarrpeulicke@gmail.com>
2019-07-27 16:48:26 +0200

    test

--- index.js
+++ index.js
@@ -10,3 +10,16 @@ app.use(bodyParser.json({limit: '10mb', extended: true}));
 app.use(bodyParser.urlencoded({limit: '10mb', extended: true, parameterLimit: 10000}));
 
 app.listen(process.env.PORT || 1337);
+
+
+
+const WebSocket = require('ws')
+
+const wss = new WebSocket.Server({ port: 8080 })
+
+wss.on('connection', ws => {
+    ws.on('message', message => {
+        console.log('Received message => ${message}')
+    })
+    ws.send('Hello! Message From Server!!')
+})
