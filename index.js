6856f1cadfdcf72adf45134ce27c691e876568d1
Oscar Peulicke <oscarrpeulicke@gmail.com>
2019-07-27 17:22:12 +0200

    test

--- index.js
+++ index.js
@@ -1,19 +1,16 @@
 var express = require('express');
 var http = require('http');
-var ws = require('ws');
+var WebSocket = require('ws');
 
 var app = express();
 app.use(express.static(__dirname + '/www'));
 
 var server = http.createServer(app);
-var wss = new ws.Server({server});
+var wss = new WebSocket.Server({server});
 
 wss.on('connection', function(ws) {
-    var id = setInterval(function() {
-        ws.send(JSON.stringify({test: "test"}));
-    }, 100);
-    ws.on('close', function() {
-        clearInterval(id);
+    ws.on('message', function(event) {
+        ws.send(event.data);
     });
 });
 
