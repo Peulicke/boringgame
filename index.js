8ca8822898a8f47d6aec541ade5649376b4da856
Oscar Peulicke <oscarrpeulicke@gmail.com>
2019-07-29 12:12:25 +0200

    test

--- index.js
+++ index.js
@@ -97,4 +97,8 @@ wss.on('connection', function(ws) {
     });
 });
 
-server.listen(process.env.PORT || 8080);
+var port = process.env.PORT || 5000;
+
+server.listen(port, function(){
+    console.log("Server listening on port " + port);
+});
