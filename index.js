2c45e9c4585233f45514eb6872cac519b588d1af
Oscar Peulicke <oscarrpeulicke@gmail.com>
2019-08-03 14:48:52 +0200

    smaller id

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ wss.getUniqueID = function () {
     function s4() {
         return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
     }
-    return s4()+s4()+s4();
+    return s4();
 };
 
 var levelSize = 1000;
